3921277372d6f5d20de4129218a166af08c2ace3
philippe mallet <you@example.com>
2024-10-11 10:38:18 +0200

    version2

--- routes/Autorisation.js
+++ routes/Autorisation.js
@@ -4,18 +4,19 @@ import { AutorisationController } from '../controllers/Autorisation.js';
 const autorisation = express.Router();
 
 // Récupérer toutes les autorisations
-autorisation.get('/autorisation', AutorisationController.getAllAutorisations);
+autorisation.get('/', AutorisationController.getAllAutorisations);
 
 // Récupérer une autorisation par ID
-autorisation.get('/autorisation/:id', AutorisationController.getAutorisationById);
+autorisation.get('/:id', AutorisationController.getAutorisationById);
 
 // Créer une nouvelle autorisation
-autorisation.post('/autorisation', AutorisationController.createAutorisation);
+autorisation.post('/', AutorisationController.createAutorisation);
 
 // Mettre à jour une autorisation existante
-autorisation.put('/autorisation/:id', AutorisationController.updateAutorisation);
+autorisation.put('/:id', AutorisationController.updateAutorisation);
 
 // Supprimer une autorisation
-autorisation.delete('/autorisation/:id', AutorisationController.deleteAutorisation);
+autorisation.delete('/:id', AutorisationController.deleteAutorisation);
 
 export default autorisation;
+
--- routes/User.js
+++ routes/User.js
@@ -3,10 +3,22 @@ import { UserController } from '../controllers/User.js';
 
 const user = express.Router();
 
-user.get('/administrateur', UserController.getAllUsers);
-user.get('/administrateur/:id', UserController.getUserById);
-user.post('/administrateur', UserController.createUser);
-user.put('/administrateur/:id', UserController.updateUser);
-user.delete('/administrateur/:id', UserController.deleteUser);
+// Récupérer tous les administrateurs
+user.get('/', UserController.getAllUsers);
+
+// Récupérer un administrateur par ID
+user.get('/:id', UserController.getUserById);
+
+// Créer un nouvel administrateur
+user.post('/', UserController.createUser);
+
+// Mettre à jour un administrateur existant
+user.put('/:id', UserController.updateUser);
+
+// Supprimer un administrateur
+user.delete('/:id', UserController.deleteUser);
+
+
 
 export default user;
+
